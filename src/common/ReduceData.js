d56211f6c5dd22865b817298d0f7607a28246691
CccGm <mihirghelani.cyber@gmail.com>
2024-01-24 13:01:25 +0530

    Date Formate Change

--- src/common/ReduceData.js
+++ src/common/ReduceData.js
@@ -3,6 +3,19 @@ import React from 'react';
 import GestureRecognizer from 'react-native-swipe-gestures';
 
 const ReduceData = ({data, onSwipe}) => {
+  const DATE = date => {
+    const today = new Date(date);
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1;
+    let dd = today.getDate();
+
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+
+    const formattedToday = dd + '/' + mm + '/' + yyyy;
+    return formattedToday;
+  };
+
   return (
     <GestureRecognizer
       onSwipeLeft={() => onSwipe(data.order_id)}
@@ -20,7 +33,7 @@ const ReduceData = ({data, onSwipe}) => {
       <Text>ID : {data.order_id}</Text>
       <Text>Seq No :{data.sequence_no}</Text>
       <Text>Order Type :{data.order_type}</Text>
-      <Text>Date : {data.expected_date}</Text>
+      <Text>Date : {DATE(data.expected_date)}</Text>
     </GestureRecognizer>
   );
 };
